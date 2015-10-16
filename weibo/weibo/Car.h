6896ac9090beb6d7ddc4c8b3b32bb76d55195c0c
Xander <unclex@sina.com>
2015-10-16 17:24:33 +0800

    第二次更改

---
+++ weibo/weibo/Car.h
@@ -0,0 +1,14 @@
+//
+//  Car.h
+//  weibo
+//
+//  Created by wangxiao on 15/10/16.
+//  Copyright © 2015年 wangxiao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+@property (nonatomic,strong)Car * c1;
+
+@end
---
+++ weibo/weibo/Car.m
@@ -0,0 +1,15 @@
+//
+//  Car.m
+//  weibo
+//
+//  Created by wangxiao on 15/10/16.
+//  Copyright © 2015年 wangxiao. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+-(void)setC1:(Car *)c1{
+    _c1 = c1;
+}
+@end
